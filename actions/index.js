31209494a3dd7720a565ae6f8b58b971fd82dff0
Mateusz Milak <mmilak197@gmail.com>
2015-06-15 18:41:49 +0200

    dodanie funkcji upload do umieszczenia pliku na s3

--- actions/index.js
+++ actions/index.js
@@ -1,6 +1,24 @@
 
 var helpers = require("../helpers");
+var template = "index.ejs";
+var S3Form = require("../s3post").S3Form;
+var Policy = require("../s3post").Policy;
+var POLICY_FILE = "policy.json";
+var AWS = require("aws-sdk");
+var configFilePath = "config.json";
 
 exports.action = function(request, callback) {
 	callback(null, "wywolanie");
+	var policyData = helpers.readJSONFile(POLICY_FILE);
+	var policy = new Policy(policyData);
+	var s3Form = new S3Form(policy);
+	var fields = s3Form.generateS3FormFields();
+	var awsConfig  = new AWS.EC2MetadataCredentials();
+	awsConfig.refresh(function(err){
+		if(err){
+			AWS.config.loadFromPath(configFilePath);
+			awsConfig = helpers.readJSONFile(configFilePath);								
+		}
+		callback(null, {template: template, params:{fields:s3Form.addS3CredientalsFields(fields, awsConfig) , bucket:"lab4-weeia"}});
+	});
 }
