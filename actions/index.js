3f81c5bbe7b68a222ac5977cf72566001d91706f
Mateusz Milak <mmilak197@gmail.com>
2015-06-22 16:22:35 +0200

    Poprawki upload plikow

--- actions/index.js
+++ actions/index.js
@@ -1,29 +1,30 @@
-
+var util = require("util");
 var helpers = require("../helpers");
-var template = "index.ejs";
-var S3Form = require("../s3post").S3Form;
 var Policy = require("../s3post").Policy;
+var S3Form = require("../s3post").S3Form;
+var AWS_CONFIG_FILE = "config.json";
 var POLICY_FILE = "policy.json";
-var AWS = require("aws-sdk");
-var configFilePath = "config.json";
+var INDEX_TEMPLATE = "index.ejs";
+var DIGEST_TEMPLATE = "digets.ejs";
 
-exports.action = function(request, callback) {
-	callback(null, "wywolanie");
+
+var task = function(request, callback){
+	//1. load configuration
+	var awsConfig = helpers.readJSONFile(AWS_CONFIG_FILE);
 	var policyData = helpers.readJSONFile(POLICY_FILE);
+
+	//2. prepare policy
 	var policy = new Policy(policyData);
+
+	//3. generate form fields for S3 POST
 	var s3Form = new S3Form(policy);
+	//4. get bucket name
+	var fields = s3Form.generateS3FormFields();
+
+	fields = s3Form.addS3CredientalsFields(fields, awsConfig);
 	
-	var awsConfig  = new AWS.EC2MetadataCredentials();
-	awsConfig.refresh(function(err){
-		if(err){
-			AWS.config.loadFromPath(configFilePath);
-			awsConfig = helpers.readJSONFile(configFilePath);								
-		}
-		
-		helpers.getCurrentIP(function(ip){
-			console.log("Aktualna domena: "+ip);
-			var fields = s3Form.generateS3FormFields(ip);
-			callback(null, {template: template, params:{fields:s3Form.addS3CredientalsFields(fields, awsConfig, ip) , bucket:"lab4-weeia"}});
-		});
-	});
+	callback(null, {template: INDEX_TEMPLATE, params:{fields:fields, bucket:"lab4-weeia"}});
+
 }
+
+exports.action = task;
